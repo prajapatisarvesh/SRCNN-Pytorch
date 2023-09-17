2d9384f31ddcb3f489cc4aa1c0af68bfef5bf5ec
prajapatisarvesh <sarvesh101p@gmail.com>
2023-09-17 18:01:22 -0400

    div2k-downloader

--- utils/div2k_downloader.py
+++ utils/div2k_downloader.py
@@ -1,7 +1,10 @@
+'''
+'''
 import os
 import requests
 import wget
-import pandas as pd
+import csv
+import zipfile
 
 
 if __name__ == '__main__':
@@ -9,11 +12,71 @@ if __name__ == '__main__':
     if 'data' not in os.listdir():
         os.mkdir('data')
     os.chdir('data')
+    if 'div2k' not in os.listdir():
+        os.mkdir('div2k')
+    os.chdir('div2k')
     urls = ['http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X2.zip', \
             'http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X2.zip', \
             'http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip', \
             'http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip']
     
+    training_data = {}
+    validation_data = {}
+    hr_train = []
+    lr_train = []
+    hr_valid = []
+    lr_valid = []
+
     for url in urls:
-        file_name = wget(urls)
+        file_name = url.split('/')[-1]
+        folder_dir = file_name.split('.')[0]
+        try:
+            if (file_name) not in os.listdir() and (folder_dir not in os.listdir() and folder_dir[:-3] not in os.listdir()):
+                print("[+] Downloading", file_name)
+                file_name = wget.download(url)
+        except Exception as e:
+            print("[-] Error downloading file")
+        if folder_dir not in os.listdir() and folder_dir[:-3] not in os.listdir():
+            print("[+] Extracting to", folder_dir)
+            try:
+                with zipfile.ZipFile(file_name, 'r') as zip:
+                    zip.extractall('.')
+                print("[+] Extraction Complete")
+            except Exception as e:
+                print("[-] Error Extraction")
+            print("[+]Cleaning up ", file_name)
+            try:
+                os.remove(file_name)
+            except Exception as e:
+                pass
+    for folder in os.listdir():
+        cwd = os.getcwd()
+        if 'div2k' in folder.lower():
+            if 'valid' in folder.lower():
+                if 'lr' in folder.lower():
+                    lr_valid = sorted(os.listdir(f'{folder}/X2'))
+                    lr_valid = [f'{cwd}/{folder}/X2/{a}' for a in lr_valid]
+                else:
+                    hr_valid = sorted(os.listdir(f'{folder}'))
+                    hr_valid = [f'{cwd}/{folder}/{a}' for a in hr_valid]
+            elif 'train' in folder.lower():
+                if 'lr' in folder.lower():
+                    lr_train = sorted(os.listdir(f'{folder}/X2'))
+                    lr_train = [f'{cwd}/{folder}/X2/{a}' for a in lr_train]
+                else:
+                    hr_train = sorted(os.listdir(f'{folder}'))
+                    hr_train = [f'{cwd}/{folder}/{a}' for a in hr_train]
+    
+    training_data = {**{"lr":"hr"}, **dict(zip(lr_train, hr_train))}
+    valid_data = {**{"lr":"hr"}, **dict(zip(lr_valid, hr_valid))}
+    with open('train.csv', 'w') as csv_:
+        writer = csv.writer(csv_)
+        for key, value in training_data.items():
+            writer.writerow([key, value])
+    
+    with open('valid.csv', 'w') as csv_:
+        writer = csv.writer(csv_)
+        for key, value in valid_data.items():
+            writer.writerow([key, value])
         
+    
