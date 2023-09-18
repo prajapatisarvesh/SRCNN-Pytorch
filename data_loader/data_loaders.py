d6c9b8179d4904b28e478387448db977e07ff15e
Sarvesh Prajapati <sarvesh101p@gmail.com>
2023-09-17 21:58:57 -0400

    bicubic-interpolation-with-model

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,21 +1,23 @@
 from base.base_data_loader import BaseDataLoader
 from torchvision import datasets, transforms
 from skimage import io
-
+import cv2
 
 class Div2kDataLoader(BaseDataLoader):
-    def __init__(self, csv_file, root_dir, transform=None):
+    def __init__(self, csv_file, root_dir, scale=2, transform=None):
         super().__init__(csv_file=csv_file, root_dir=root_dir, transform=transform)
+        self.scale = scale
         print("[+] Data Loaded with rows: ", super().__len__())
 
 
     def __getitem__(self, idx):
         lr_image_name = self.csv_dataframe.iloc[idx, 0]
         hr_image_name = self.csv_dataframe.iloc[idx, 1]
-        lr_image = io.imread(lr_image_name)
-        hr_image = io.imread(hr_image_name)
+        lr_image = cv2.imread(lr_image_name)
+        hr_image = cv2.imread(hr_image_name)
+        ### Bicubic Interpolate LR Image
+        lr_image = cv2.resize(lr_image, (lr_image.shape[1]*self.scale, lr_image.shape[0]*self.scale), interpolation=cv2.INTER_CUBIC)
         sample = {'lr_image':lr_image, 'hr_image': hr_image}
         if self.transform:
             sample = self.transform(sample)
-        
         return sample
--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 from data_loader import data_loaders
 import os
-
+from torch.utils.data import DataLoader
+import cv2
 
 if __name__ == '__main__':
     data = data_loaders.Div2kDataLoader('train.csv', os.getcwd())
+    
