f92eed4187c093749287952a67f088e42b450ab8
Abhinav kumar <kumar.abhina@northeastern.edu>
2023-09-18 22:48:28 -0400

    fix-model-kernels

--- model/model.py
+++ model/model.py
@@ -7,7 +7,7 @@ class SRCNN(BaseModel):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=9, padding=4)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=1, padding=0)
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=3, kernel_size=5, padding=2)
 
     
